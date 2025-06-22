7f18a6c6835e5656e3b63a3542e8d2a00487e010
Pulkit Rathi <pulkit.rathi@oracle.com>
2025-06-22 23:50:41 +0530

    Add proxy pattern comment

--- src/main/java/design/patterns/structural/proxy/Demo.java
+++ src/main/java/design/patterns/structural/proxy/Demo.java
@@ -2,6 +2,19 @@ package design.patterns.structural.proxy;
 
 // https://www.youtube.com/watch?v=TS5i-uPXLs8
 
+/*
+A Proxy is a class that acts as an interface to something else: another object, a resource, or a remote service.
+It wraps the real object and adds extra behavior (like logging, caching, access control, lazy loading, etc.) without changing the original object.
+
+Below piece of code in dataflow_cp is example of proxy pattern specifically dynamic proxy.
+A dynamic proxy is a runtime-generated class that implements one or more interfaces and delegates method calls to an InvocationHandler via Java Reflection
+
+(KubernetesClient) Proxy.newProxyInstance(this.getClass().getClassLoader(),
+          new Class[]{KubernetesClient.class},
+          new MetricsInvocationHandler(
+              new DefaultKubernetesClient(customHttpClient, kubeTokenClientConfig)));
+*/
+
 public class Demo {
   public static void main(String[] args) {
     Implementation imp = new Implementation();
