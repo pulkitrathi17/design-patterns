2463973552304c354f24853b89ecd517e4e5907e
pulkitrathi17 <pulkitrathi98@gmail.com>
2025-06-29 13:00:40 +0530

    Add observer pattern comments

--- src/main/java/design/patterns/behavioral/observer/Demo.java
+++ src/main/java/design/patterns/behavioral/observer/Demo.java
@@ -3,6 +3,11 @@ package design.patterns.behavioral.observer;
 import design.patterns.behavioral.observer.listerners.EmailListener;
 import design.patterns.behavioral.observer.listerners.MobileAppListener;
 
+// Behavioral design pattern where one object (Subject) maintains a list of dependents (Observers)
+// and notifies them automatically of any state changes, usually by calling one of their methods.
+
+// Use cases: Useful in event-driven microservices for clean pub-sub integration, File system watchers, Stock price update notifications.
+
 public class Demo {
 
   public static void main(String[] args) {
